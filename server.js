57746842e7c809fb4b245c4f7d405f708abd0893
ahmet123123947 <ahmetalgan104@gmail.com>
2025-11-28 15:55:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
 import express from "express";
 import { exec } from "child_process";
 import fs from "fs";
